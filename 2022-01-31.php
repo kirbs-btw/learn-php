ecf3621ba504297974578e057b71966ef1cab36d
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-01-31 16:22:49 +0100

    Add files via upload

--- 2022-01-31.php
+++ 2022-01-31.php
@@ -1,21 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <body>
+
 <?php
 //fiss bass
+  function main(){
+    for ($i = 0; $i <= 100; $i++) {
+      if (($i % 3 == 0) && ($i % 5 == 0)) {
+        echo "$i fissbass </br>";
+      }elseif ($i % 3 == 0){
+        echo "$i bass </br>";
 
-function main(){
-  for ($i = 0; $i <= 100; $i++) {
-    if (($i % 3 == 0) && ($i % 5 == 0)) {
-      echo "$i fissbass </br>";
-    }elseif ($i % 3 == 0){
-      echo "$i bass </br>";
-
-    } elseif ($i % 5 == 0){
-      echo "$i fiss </br>";
-    } else{
-      echo "$i </br>"
+      } elseif ($i % 5 == 0){
+        echo "$i fiss </br>";
+      } else {
+        echo "$i </br>";
+      }
     }
-}
 
-}
-main();
-
+  }
+  main();
 ?>
+
+  </body>
+</html>
