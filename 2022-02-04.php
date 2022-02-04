eb38acc8c72c4c031694354b898f5ff94bf9ee06
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-02-04 17:03:52 +0100

    Add files via upload

---
+++ 2022-02-04.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function printArray($arr){
+	// print array any len     
+	for ($x = 0; $x < count($arr); $x++){
+    	
+    	echo "$arr[$x] ";
+    
+    }
+
+}
+
+function arraysTry($arr){
+	// some funny bit to understand how 
+    // arrays work
+	
+    printArray($arr);
+    
+
+}
+
+function arraysTry2($arr){
+	$arr[4] = "not python";
+	printArray($arr);
+}
+
+
+
+function execute(){
+	$arr = array("hello", "friends", "me", "learning", 			"php");
+    arraysTry($arr);
+    echo "</br>";
+	arraysTry2($arr);
+}
+
+execute();
+
+
+?> 
+
+</body>
+</html>
