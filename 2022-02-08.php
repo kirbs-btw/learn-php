32e61a99c835beea7d933b230a8466c5f2e4f6f6
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-02-08 08:31:54 +0100

    Add files via upload

--- 2022-02-08.php
+++ 2022-02-08.php
@@ -6,6 +6,17 @@
   </head>
   <body>
     <?php
+      // 2022-02-08 - php-log:
+      // Today i tried string edits and global var
+      //
+      // Learned:
+      // -editing text is pretty straight forward
+      // -global var´s are a bit awkward to use because they miss a $ and the name
+      // is defined as a str
+      // -define() is only used for constants the value can´t be changed!
+      // -still missing semicolons :/
+
+
       //text edits
       function replaceText(){
         $text = "Hello sir this is cool!";
@@ -20,8 +31,8 @@
         echo strrev($text);
       }
 
-      //reverseStuff();
-      //replaceText();
+      reverseStuff();
+      replaceText();
 
       //constants
 
