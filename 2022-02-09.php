53cdc94b0871e742c357fb4f6480d4b6e705594e
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-02-09 14:17:13 +0100

    Add files via upload

---
+++ 2022-02-09.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	// Today i looked at: 
+    // date()
+    //	learned: 
+   	// Y = 2022
+    // y = 22
+    // m = 02
+    // d = 09
+    // l = Wednesday
+    
+
+define("today", date("Y-m-d"));
+
+function outDates(){
+	echo date("l");
+    echo "</br>";
+    echo date("y");
+    echo "</br>";
+    echo today;
+    echo "</br>";
+	echo date("Y-d-m");
+	
+}
+
+outDates();
+
+?>
+
+</body>
+</html>
