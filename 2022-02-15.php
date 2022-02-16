9f0895291de851083277040b477147a637f85fe3
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-02-16 17:06:21 +0100

    Add files via upload

---
+++ 2022-02-15.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      // object in object try
+      // object quarry works like
+      // $this->thing->here
+      
+
+
+      class hobby{
+        public $name;
+        public $cost;
+
+        function set_values($name, $cost){
+          $this->name = $name;
+          $this->cost = $cost;
+        }
+        function print_values(){
+          echo "$this->name </br>";
+          echo "$this->cost </br>";
+        }
+      }
+
+      class person{
+        public $name;
+        public $hobby;
+
+        function set_values($name, $hobby){
+          $this->name = $name;
+          $this->hobby = $hobby;
+        }
+        function print_values(){
+          echo "$this->name </br>";
+
+          $hobbyname = $this->hobby->name;
+          echo "$hobbyname </br>";
+        }
+      }
+
+      $tennis = new hobby();
+      $tennis->set_values("tennis", 50);
+      $tennis->print_values();
+
+      echo "</br>";
+
+      $tom = new person();
+      $tom->set_values("tom", $tennis);
+      $tom->print_values();
+
+
+    ?>
+  </body>
+</html>
